befef86159a3a17a8f1d72f0cd124c75e39ad5de
rusdinfitri <161561122+rusdinfitri@users.noreply.github.com>
2024-02-28 16:56:20 +0800

    Update Advertising.py

--- Advertising.py
+++ Advertising.py
@@ -12,11 +12,11 @@ st.sidebar.header('User Input Parameters')
 
 def user_input_features():
     TV= st.sidebar.slider('TV', 0.7, 297.0, 100.0) #all Float
-    Radio = st.sidebar.slider('Radio', 0, 50.0, 15.0)
+    Radio = st.sidebar.slider('Radio', 0.0, 50.0, 15.0)
     Newspaper = st.sidebar.slider('Newspaper', 0.3, 114.0, 20.0)
-    data = {'TV': tv,
-            'Radio': radio,
-            'Newspaper': newspaper}
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper}
     features = pd.DataFrame(data, index=[0])
     return features
 
